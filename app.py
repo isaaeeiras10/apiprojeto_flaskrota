a14f877f4cb24c9ff1bcb6daac404e295edde932
isaaeeiras10 <isabelaeiras13@gmail.com>
2024-04-16 08:35:02 -0300

    implementação de rotas

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+from funcoes import *
+from random_data import *
 
 app = Flask(__name__)
 @app.route('/')
@@ -14,6 +16,26 @@ def aleatorio():
 @app.route("/argumentos/<string:nome>")
 def argumentos(nome: str):
     return jsonify({"ststaus": 200, "data": nome})
+
+@app.route("/maior50")
+def maior50():
+    return jsonify(maior_de_50(pessoas))
+
+@app.route("/maisde2000")
+def maisde2000():
+    return jsonify(f'Ganham mais de R$2000 :{mais_2000(pessoas)[0]} pessoas, representando {mais_2000(pessoas)[1]} % de {mais_2000(pessoas)[2]} no total')
+
+@app.route("/tresmaioressalarios")
+def tresmaioressalarios():
+    return jsonify(maior_salario(pessoas))
+
+@app.route("/mediasalarialprofissao")
+def mediasalarialprofissao():
+    return jsonify(media_profissoes(pessoas))
+
+@app.route("/maior2000sexo")
+def maior2000sexo():
+    return jsonify(maior_2000_sexo(pessoas))
+
 if __name__ == '__main__':
     app.run(debug=True)
-
