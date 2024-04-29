12cc2ad5f878d6e3623ac1df7ddecdeb4ba5521c
isaaeeiras10 <isabelaeiras13@gmail.com>
2024-04-29 15:32:21 -0300

    V0.1

---
+++ distancia.py
@@ -0,0 +1,7 @@
+origem = input("Cidade da origem: ")
+destino = input("Cidade  de destino: ")
+distancia = float(input("Qual a distancia em km? "))
+velocidade = float(input("Quantos km/h irá viajar? "))
+tempo = distancia/velocidade*60
+nome_motorista = input("Digite o nome do motorista: ")
+print(f"{nome_motorista} você levará {tempo} minutos de {origem} à {destino}")
