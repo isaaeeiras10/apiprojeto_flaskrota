5f82a434a7eb143f18e2fe718bb3c023b1e62527
isaaeeiras10 <isabelaeiras13@gmail.com>
2024-05-07 08:13:19 -0300

    criando api

---
+++ api.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify, request
+from funcoes import *
+from random_data import *
+
+bp = Blueprint("api", __name__)
+
+@bp.route('/api/', methods=("GET"))
+def index():
+    return jsonify({"status": 200, "message": "API da Manuela de Lima Ribeiro Vieira"})
+@bp.route('/api/aleatorio', methods=("GET"))
+def aleatorios():
+    import random
+    a = random.randint(49, 100)
+    return jsonify({"status": 200, "data": a})
+
+@bp.route('/api/argumentos/<string:nome>', methods=("GET"))
+def argumentos(nome:str):
+    return jsonify({"status": 200, "data":nome})
+
+@bp.route('/api/maior50', methods=("GET"))
+def maior50():
+    return jsonify(maior_de_50(pessoas))
+
+@bp.route('/api/mais2000', methods=("GET"))
+def mais2000():
+    return jsonify(f'Ganham mais de R$2000 :{mais_2000(pessoas)[0]} pessoas, representando {mais_2000(pessoas)[1]} % de {mais_2000(pessoas)[2]} no total')
+
+@bp.route('/api/tresmaioressalarios', methods=("GET"))
+def tresmaioressalarios():
+    return jsonify(maior_salario(pessoas, maior=None))
+
+@bp.route('/api/mediasalarioprofissao', methods=("GET"))
+def mediasalarioprofissao():
+    return jsonify(media_profissoes(pessoas))
+
+@bp.route('/api/intervalopessoas2000', methods=("GET"))
+def intervalopessoas2000():
+    return jsonify(maior_2000_sexo(pessoas, sexo='Masculoino'), maior_2000_sexo(pessoas, sexo='Feminino'))
--- app.py
+++ app.py
@@ -1,41 +1,10 @@
 from flask import Flask, jsonify
+import random
 from funcoes import *
 from random_data import *
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return jsonify({"status": 200, "message": "API da Isabela Canôas da Silva Eiras"})
 
-@app.route("/aleatorios")
-def aleatorio():
-    import random
-    a = random.randint(49, 100)
-    return jsonify({"status": 200, "data": a})
-
-@app.route("/argumentos/<string:nome>")
-def argumentos(nome: str):
-    return jsonify({"ststaus": 200, "data": nome})
-
-@app.route("/maior50")
-def maior50():
-    return jsonify(maior_de_50(pessoas))
-
-@app.route("/maisde2000")
-def maisde2000():
-    return jsonify(f'Ganham mais de R$2000 :{mais_2000(pessoas)[0]} pessoas, representando {mais_2000(pessoas)[1]} % de {mais_2000(pessoas)[2]} no total')
-
-@app.route("/tresmaioressalarios")
-def tresmaioressalarios():
-    return jsonify(maior_salario(pessoas, maior=None))
-
-@app.route("/mediasalarialprofissao")
-def mediasalarialprofissao():
-    return jsonify(media_profissoes(pessoas))
-
-@app.route("/maior2000sexo")
-def maior2000sexo():
-    return jsonify(maior_2000_sexo(pessoas, sexo='Masculino'), maior_2000_sexo(pessoas, sexo='Feminino'))
 
 if __name__ == '__main__':
     app.run(debug=True)
