1b098320c77e4aeb0cefb99bcedbfb0ac5b7de49
isaaeeiras10 <isabelaeiras13@gmail.com>
2024-05-07 10:57:35 -0300

    html pronto

--- api.py
+++ api.py
@@ -4,35 +4,35 @@ from random_data import *
 
 bp = Blueprint("api", __name__)
 
-@bp.route('/api/', methods=("GET"))
+@bp.route('/api', methods=("GET", ))
 def index():
-    return jsonify({"status": 200, "message": "API da Manuela de Lima Ribeiro Vieira"})
-@bp.route('/api/aleatorio', methods=("GET"))
+    return jsonify({"status": 200, "message": "API da Isabela Canoas Silva Eiras"})
+@bp.route('/api/aleatorios', methods=("GET", ))
 def aleatorios():
     import random
     a = random.randint(49, 100)
     return jsonify({"status": 200, "data": a})
 
-@bp.route('/api/argumentos/<string:nome>', methods=("GET"))
+@bp.route('/api/argumentos/<string:nome>', methods=("GET", ))
 def argumentos(nome:str):
     return jsonify({"status": 200, "data":nome})
 
-@bp.route('/api/maior50', methods=("GET"))
-def maior50():
+@bp.route('/api/idades', methods=("GET", ))
+def idades():
     return jsonify(maior_de_50(pessoas))
 
-@bp.route('/api/mais2000', methods=("GET"))
+@bp.route('/api/mais2000', methods=("GET", ))
 def mais2000():
     return jsonify(f'Ganham mais de R$2000 :{mais_2000(pessoas)[0]} pessoas, representando {mais_2000(pessoas)[1]} % de {mais_2000(pessoas)[2]} no total')
 
-@bp.route('/api/tresmaioressalarios', methods=("GET"))
+@bp.route('/api/tresmaioressalarios', methods=("GET", ))
 def tresmaioressalarios():
     return jsonify(maior_salario(pessoas, maior=None))
 
-@bp.route('/api/mediasalarioprofissao', methods=("GET"))
+@bp.route('/api/mediasalarialprofissao', methods=("GET", ))
 def mediasalarioprofissao():
     return jsonify(media_profissoes(pessoas))
 
-@bp.route('/api/intervalopessoas2000', methods=("GET"))
+@bp.route('/api/intervalodepessoas2000', methods=("GET", ))
 def intervalopessoas2000():
     return jsonify(maior_2000_sexo(pessoas, sexo='Masculoino'), maior_2000_sexo(pessoas, sexo='Feminino'))
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from random_data import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=("GET", ))
 def index():
     return render_template("index.html")
 
